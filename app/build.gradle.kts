38056f28242e39fa8a9682496efe98a89b7e7769
Eric Sheesley <esheesle@macbookair-m4.home.swlr.net>
2026-05-04 08:40:38 -0400

    Move Watchdog into separate space to avoid cancellation (v1.2.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "net.swlr.vpnmaster"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 9
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
